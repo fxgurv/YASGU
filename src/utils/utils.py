36ce1774290d46a99bd46ee594cba7b8dc75d29a
Strange <168190107+fxgurv@users.noreply.github.com>
2024-07-08 04:43:59 -0700

    Update utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,72 +1,36 @@
 import os
 import platform
 import random
-
 from srt_equalizer import srt_equalizer
-
 from src.utils.config import ROOT_DIR
 from src.utils.status import *
 
 
 def close_running_selenium_instances() -> None:
-    """
-    Closes any running Selenium instances.
-
-    Returns:
-        None
-    """
     try:
         info(" => Closing running Selenium instances...")
-
-        # Kill all running Firefox instances
         if platform.system() == "Windows":
             os.system("taskkill /f /im firefox.exe")
         else:
             os.system("pkill firefox")
-
         success(" => Closed running Selenium instances.")
-
     except Exception as e:
         error(f"Error occurred while closing running Selenium instances: {str(e)}")
 
 
 def build_url(youtube_video_id: str) -> str:
-    """
-    Builds the URL to the YouTube video.
-
-    Args:
-        youtube_video_id (str): The YouTube video ID.
-
-    Returns:
-        url (str): The URL to the YouTube video.
-    """
     return f"https://www.youtube.com/watch?v={youtube_video_id}"
 
 
 def rem_temp_files() -> None:
-    """
-    Removes temporary files in the `temp` directory.
-
-    Returns:
-        None
-    """
-    # Path to the `temp` directory
     mp_dir = os.path.join(ROOT_DIR, "temp")
-
     files = os.listdir(mp_dir)
-
     for file in files:
         if not file.endswith(".json"):
             os.remove(os.path.join(mp_dir, file))
 
 
 def choose_random_song() -> str:
-    """
-    Chooses a random song from the songs/ directory.
-
-    Returns:
-        str: The path to the chosen song.
-    """
     try:
         songs = os.listdir(os.path.join(ROOT_DIR, "assets/songs"))
         song = random.choice(songs)
@@ -77,14 +41,4 @@ def choose_random_song() -> str:
 
 
 def equalize_subtitles(srt_path: str, max_chars: int = 10) -> None:
-    """
-    Equalizes the subtitles in a SRT file.
-
-    Args:
-        srt_path (str): The path to the SRT file
-        max_chars (int): The maximum amount of characters in a subtitle
-
-    Returns:
-        None
-    """
     srt_equalizer.equalize_srt_file(srt_path, srt_path, max_chars)
